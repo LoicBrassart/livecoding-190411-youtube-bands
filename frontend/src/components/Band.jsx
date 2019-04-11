53b18a32b48b8bd449d32c1226f6ea2eb69f6829
LoicBrassart <loic.brassart@gmail.com>
2019-04-11 11:34:36 +0200

    Mega-commit:

--- frontend/src/components/Band.jsx
+++ frontend/src/components/Band.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import axios from "axios";
+import Youtube from "react-youtube";
 
 class Band extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoId: ""
+    };
+  }
+
+  componentDidMount() {
+    const apiKey = "AIzaSyAYQjF7_hRZGUMoUwlcUezlq33cGFz5SO0";
+    const query = `${this.props.bandData.title} band`;
+
+    axios
+      .create({
+        baseURL: "https://content.googleapis.com/youtube/v3",
+        timeout: 1000
+      })
+      .get(
+        `/search?q=${query}&part=snippet&maxResults=1&type=video&key=${apiKey}`
+      )
+      .then(response => {
+        const vidId = response.data.items[0].id.videoId;
+        this.setState({ videoId: vidId });
+      })
+      .catch(function(error) {
+        console.log("Echec appel API Youtube: " + error);
+      });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <p>
+        <h2>
           {this.props.bandData.title} (by {this.props.bandData.author})
-        </p>
+        </h2>
+        <Youtube videoId={this.state.videoId} />
       </React.Fragment>
     );
   }
