fda5a5ed8392785ad79640f2f7fb7491d5c2a66c
LoicBrassart <loic.brassart@gmail.com>
2019-05-13 19:58:28 +0200

    Updates

---
+++ precache-manifest.a5769bccd4d6e79b34b45034bac7da4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/livecoding-190411-youtube-bands/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dccc7a6fad224db204fe",
+    "url": "/livecoding-190411-youtube-bands/static/js/runtime~main.8ffcb4c3.js"
+  },
+  {
+    "revision": "741f9a3f5114b0c0cb17",
+    "url": "/livecoding-190411-youtube-bands/static/js/main.b7d97ed5.chunk.js"
+  },
+  {
+    "revision": "0e90d2f5ead65cf8f544",
+    "url": "/livecoding-190411-youtube-bands/static/js/2.287739f3.chunk.js"
+  },
+  {
+    "revision": "741f9a3f5114b0c0cb17",
+    "url": "/livecoding-190411-youtube-bands/static/css/main.56d19f11.chunk.css"
+  },
+  {
+    "revision": "6f3d5bb9eb46ad9854124f540187521d",
+    "url": "/livecoding-190411-youtube-bands/index.html"
+  }
+];
